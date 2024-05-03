4fde4332f3babc252bd5a81f4a2542d5db748f28
TX3656 <tx3656@U-WSD9KWO5NP6R.prod2525.local>
2024-05-03 23:04:53 +0300

    added my own

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -12,6 +12,8 @@
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-surround',
+  'tpope/vim-fugitive',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -30,7 +32,7 @@ require('lazy').setup({
 
   require 'kickstart/plugins/gitsigns',
 
-  require 'kickstart/plugins/which-key',
+  -- require 'kickstart/plugins/which-key',
 
   require 'kickstart/plugins/telescope',
 
