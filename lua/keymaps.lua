5fb9071cfd722fb7cf1db78aa33fdf9417bc69d4
TX3656 <tx3656@U-WSD9KWO5NP6R.prod2525.local>
2024-05-03 23:41:50 +0300

    better completion

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -20,10 +20,11 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Move up and center text' })
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Move down and center text' })
 vim.keymap.set('i', 'jj', '<Esc>', { desc = 'Escape' })
